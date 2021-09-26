e01753d91776f2d6446a395a000ec75a9b82a69f
Denis Komarkovu <soundze@mail.ru>
2021-09-26 20:51:50 +0300

    Working with admin panel

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150,verbose_name='Наименование')
+    content = models.TextField(blank=True,verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='Опубликовано')
+    updated_at = models.DateTimeField(auto_now=True,verbose_name='Изменено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    is_published = models.BooleanField(default=True,verbose_name='Активно')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import News
 
 def index(request):
-    return HttpResponse('Hello world')
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news':news,
+        'title':'Список новостей'}
+    return render(request,'news/index.html',context)
